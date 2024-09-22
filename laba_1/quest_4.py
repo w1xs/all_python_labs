f9549d2ef99f79de759206c799ce1d33928e9933
w1xs <ddmalyshev@yandex.ru>
2024-09-22 21:40:16 +0300

    laba 1 fix

--- laba_1/quest_4.py
+++ laba_1/quest_4.py
@@ -1,9 +1,17 @@
+def check_for_correct_input(string: str):
+    for char in string:
+        if char not in '.-0123456789':
+            return False
+    return True
+
+
 input_x_coord = input("Введите х координату вектора: ")
 input_y_coord = input("Введите y координату вектора: ")
 input_z_coord = input("Введите z координату вектора: ")
 
-if input_x_coord.isdigit() and input_y_coord.isdigit() and input_z_coord.isdigit():
-    vector_length = (int(input_x_coord)**2 + int(input_y_coord)**2 + int(input_z_coord)**2)**0.5
+if check_for_correct_input(input_x_coord) and check_for_correct_input(input_z_coord) and check_for_correct_input(
+        input_z_coord):
+    vector_length = (int(input_x_coord) ** 2 + int(input_y_coord) ** 2 + int(input_z_coord) ** 2) ** 0.5
     print(f"Длина заданного вектора: {vector_length}")
 else:
     print("Введены неверные данные")
